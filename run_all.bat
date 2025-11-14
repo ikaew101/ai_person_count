1b5756b76500940122cf138a9b58b815539bce7f
Piyanat Wongwichan <ikaew32bit@gmail.com>
2025-11-14 15:53:56 +0700

    20251114

--- run_all.bat
+++ run_all.bat
@@ -1,40 +1,26 @@
 @echo off
 echo Starting batch processing...
 
-REM --- 5129_Banglen-Yao ---
+REM --- Download Raw Data (File Video, Camera raw excel) ---
 echo.
 echo ===========================================
-echo Processing Camera: 5129_Banglen-Yao (Full Video)
+echo Processing Download Raw Data (File Video, Camera raw excel)
 echo ===========================================
-python ai_personCount.py 5129_Banglen-Yao --video_hour 19 --start_min 0 --duration_min 1
+python download_rawdata.py
 
-REM --- 5033_entrance2 ---
+REM --- Check Status File ---
 echo.
 echo ===========================================
-echo Processing Camera: 5033_entrance2 (Start: 0 min, Duration: 30 min)
+echo Processing Check Status File
 echo ===========================================
-python ai_personCount.py 5033_entrance2 --video_hour 19 --start_min 0 --duration_min 1
+python generate_master_log.py
 
-REM --- Rawai2-CAM24 ---
+REM --- Run Processing AI QA Camera ---
 echo.
 echo ===========================================
-echo Processing Camera: Rawai2-CAM24 (Start: 30 min, Duration: 30 min)
+echo Run Processing AI QA Camera
 echo ===========================================
-python ai_personCount.py 166_rawai2-cam24 --video_hour 19 --start_min 0 --duration_min 1
-
-REM --- lampun-19_old ---
-echo.
-echo ===========================================
-echo Processing Camera: lampun-19_old (Full Video)
-echo ===========================================
-python ai_personCount.py lampun-19_old --video_hour 19 --start_min 0 --duration_min 1
-
-REM --- lampun-19_new ---
-echo.
-echo ===========================================
-echo Processing Camera: lampun-19_new (Full Video)
-echo ===========================================
-python ai_personCount.py lampun-19_new --video_hour 19 --start_min 0 --duration_min 1
+python run_processor.py
 
 echo.
 echo ===========================================
